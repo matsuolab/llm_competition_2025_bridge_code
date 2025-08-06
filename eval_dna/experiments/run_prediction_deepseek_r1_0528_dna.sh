e58b3f7d7268dbe8ea4219c8fb300f6585dc8ed5
kumapo <kumapo@users.noreply.github.com>
2025-08-06 16:18:15 +0900

    need run_prediction_deepseek_r1_0528_dna.sh

--- eval_dna/experiments/run_prediction_deepseek_r1_0528_dna.sh
+++ eval_dna/experiments/run_prediction_deepseek_r1_0528_dna.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-#SBATCH --job-name=qwen3_235b_dna_8gpu
+#SBATCH --job-name=predict_deepseek_r1_0528_hle_8gpu
 #SBATCH --partition=P06
-#SBATCH --nodelist=osk-gpu[67,68]
+#SBATCH --nodelist=osk-gpu[66,68]
 #SBATCH --nodes=2
 #SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-task=240
 #SBATCH --time=04:00:00
-#SBATCH --output=eval_dna/logs/%x-%j-%t.out
-#SBATCH --error=eval_dna/logs/%x-%j-%t.err
-#SBATCH --export=OPENAI_API_KEY="<openai_api_keyをここに>",HF_TOKEN="<huggingface_tokenをここに>"
+#SBATCH --output=eval_hle/logs/%x-%j.out
+#SBATCH --error=eval_hle/logs/%x-%j.err
+
+#--- 作業ディレクトリ & logs --------------------------------------------
+export EVAL_DIR="eval_hle"
+mkdir -p "$EVAL_DIR/logs"
+echo "log dir : $EVAL_DIR/logs"
+
 #--- モジュール & Conda --------------------------------------------
 module purge
 module load cuda/12.6 miniconda/24.7.1-py312
@@ -17,15 +22,14 @@ module load nccl/2.24.3
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate llmbench
 
+# Hugging Face 認証
+# secrets.env.exampleファイルを自分のトークンに置き換えてください
+source $EVAL_DIR/secrets.env
+
 export HF_HOME=${SLURM_TMPDIR:-$HOME}/.hf_cache
-export TRANSFORMERS_CACHE=$HF_HOME
 export HUGGINGFACE_HUB_TOKEN=$HF_TOKEN
 mkdir -p "$HF_HOME"
 echo "HF cache dir : $HF_HOME"
-
-export EVAL_DIR="eval_dna"
-mkdir -p "$EVAL_DIR/logs"
-echo "log dir : $EVAL_DIR/logs"
 
 export PYTHONUNBUFFERED=1
 export RAY_DISABLE_IMPORT_WARNING=1
@@ -42,28 +46,31 @@ echo "NODE_LIST: $SLURM_JOB_NODELIST"
 nvidia-smi -i 0,1,2,3,4,5,6,7 -l 3 > $EVAL_DIR/logs/nvidia-smi.log &
 pid_nvsmi=$!
 
-# export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-export MASTER_IP=192.168.1.67
-echo "Master node: ($MASTER_IP)"
-# export VLLM_HOST_IP=$(hostname -I | awk '{print $1}')
-# echo "VLLM_HOST_IP: $VLLM_HOST_IP"  
+export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+export MASTER_IP=192.168.1.66
+echo "Master node: $MASTER_ADDR ($MASTER_IP)"
+export VLLM_HOST_IP=$(hostname -I | awk '{print $1}')
+echo "VLLM_HOST_IP: $VLLM_HOST_IP"  
 
 #--- vLLM 起動（自動Ray設定）---------------------------------------
 if [ $SLURM_PROCID -eq 0 ]; then
-  echo "Master node is starting ray..."
-  ray start --head --port=6379 --dashboard-host=0.0.0.0 --node-ip-address=$MASTER_IP
+  ray start --head --port=6379 --dashboard-host=0.0.0.0 --node-ip-address=$VLLM_HOST_IP
+
   echo "Master node waiting for worker to join..."  
   sleep 60
+
   ray status
 
-  vllm serve Qwen/Qwen3-235B-A22B \
+  # https://github.com/vllm-project/vllm/blob/f5d0f4784fdd93f1032f3bb81220af10d7588f5a/examples/online_serving/ray_serve_deepseek.py
+  vllm serve deepseek-ai/DeepSeek-R1-0528 \
     --tensor-parallel-size 8 \
     --pipeline-parallel-size 2 \
     --distributed-executor-backend ray \
-    --reasoning-parser qwen3 \
-    --rope-scaling '{"rope_type":"yarn","factor":4.0,"original_max_position_embeddings":32768}' \
+    --reasoning-parser deepseek_r1 \
     --max-model-len 131072 \
-    --gpu-memory-utilization 0.95 \
+    --gpu-memory-utilization 0.92 \
+    --max-num-seqs 256 \
+    --dtype auto \
     --trust-remote-code \
     > $EVAL_DIR/logs/vllm.log 2>&1 &
   pid_vllm=$!
@@ -78,7 +85,7 @@ if [ $SLURM_PROCID -eq 0 ]; then
 
   #--- 推論 -----------------------------------------------------------
   python $EVAL_DIR/llm-compe-eval/predict_huggingface_models.py \
-    --model_name "Qwen/Qwen3-235B-A22B" \
+    --model_name "deepseek-ai/DeepSeek-R1-0528" \
     --dataset_path "llm-2025-sahara/dna-10fold" \
     --output_dir $EVAL_DIR/evaluation_results \
     --use_vllm \
@@ -92,10 +99,7 @@ if [ $SLURM_PROCID -eq 0 ]; then
   wait $pid_vllm 2>/dev/null
   ray stop
 else
-  # 以下は workerノードで実行する
-  echo "Worker node waiting for master to start..."  
-  sleep 60
-  ray start --address=$MASTER_IP:6379 --node-ip-address=192.168.1.68
+  ray start --address=$MASTER_IP:6379 --node-ip-address=$VLLM_HOST_IP  
 
   # Master nodeが完了するまで待機
   echo "Worker node waiting for master to complete..."
