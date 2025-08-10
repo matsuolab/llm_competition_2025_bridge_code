73dce32a8e79c9d4b1d171afdbc790acd9a5ad87
kumapo <kumapo@users.noreply.github.com>
2025-08-11 00:23:16 +0900

    need run_judge_qwen3_4b_omr_genselect_peft_hle.sh

--- eval_hle/experiments/run_judge_qwen3_32b_omr_peft_hle.sh
+++ eval_hle/experiments/run_judge_qwen3_32b_omr_peft_hle.sh
@@ -40,6 +40,7 @@ vllm serve deepseek-ai/DeepSeek-R1-Distill-Llama-70B \
   --max-model-len 131072 \
   --gpu-memory-utilization 0.95 \
   --max-num-seqs 512 \
+  --max_num_batched_tokens 10240 \
   --dtype "bfloat16" \
   > $EVAL_DIR/logs/vllm.log 2>&1 &
 pid_vllm=$!
