7462fbcccd60d45370e4abfc09cfb9cdff5ed94d
kumapo <kumapo@users.noreply.github.com>
2025-08-12 00:18:07 +0900

    need run_qwen3_32b.sh

--- train/scripts/sft/open_math_reasoning_genselect/run_qwen3_32b.sh
+++ train/scripts/sft/open_math_reasoning_genselect/run_qwen3_32b.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=P06
 #SBATCH --nodelist=osk-gpu68
 #SBATCH --nodes=1
-#SBATCH --gpus-per-node=6
-#SBATCH --cpus-per-task=64
+#SBATCH --gpus-per-node=8
+#SBATCH --cpus-per-task=240
 #SBATCH --time=04:00:00
 #SBATCH --output=train/logs/%x-%j.out
 #SBATCH --error=train/logs/%x-%j.err
@@ -31,12 +31,9 @@ echo "conda dir : $CONDA_PATH"
 huggingface-cli login --token $HF_TOKEN
 wandb login
 
-nvidia-smi > nvidia-smi.log
-nvidia-smi pmon -c 1 > nvidia-smi-pmon.log
-
 export NCCL_SOCKET_IFNAME=enp25s0np0
 export NVTE_FUSED_ATTN=0
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 ulimit -v unlimited
 
 export HF_HOME=${SLURM_TMPDIR:-$HOME}/.hf_cache
@@ -59,19 +56,19 @@ echo "trainer.default_local_dir : $HOME/training/sft/open_math_reasoning_gensele
 # --standalone: 単一ノードでの実行
 # --nnodes=1: ノード数1
 # --nproc_per_node: ノードあたりのプロセス数（GPU数）
-torchrun --standalone --nnodes=1 --nproc_per_node=6 \
+torchrun --standalone --nnodes=1 --nproc_per_node=8 \
     -m verl.trainer.fsdp_sft_trainer \
     data.train_files=$HOME/data/open_math_reasoning_genselect/test.parquet \
     data.prompt_key=extra_info \
     data.response_key=extra_info \
     data.prompt_dict_keys=['question'] \
     +data.response_dict_keys=['answer'] \
-    data.train_batch_size=60 \
+    data.train_batch_size=64 \
     data.micro_batch_size_per_gpu=2 \
     model.partial_pretrain=Qwen/Qwen3-32B \
     data.max_length=8192 \
     use_remove_padding=True \
-    ulysses_sequence_parallel_size=6 \
+    ulysses_sequence_parallel_size=8 \
     data.truncation=right \
     trainer.project_name=$SLURM_JOB_NAME \
     trainer.experiment_name=$SLURM_JOB_NAME-$SLURM_JOBID \
