b7544d378c696d52e2ea55a205600d9bd4630b7d
kumapo <kumapo@users.noreply.github.com>
2025-08-19 03:16:09 +0900

    wip run_deepseek_r1_0528_peft.sh

--- train/scripts/mutinode_sft/run_deepseek_r1_0528_peft.sh
+++ train/scripts/mutinode_sft/run_deepseek_r1_0528_peft.sh
@@ -9,9 +9,10 @@ conda init
 conda config --set auto_activate_base false
 source ~/.bashrc
 
-export SLURM_JOB_NAME=deepseek_r1_0528_peft_8gpu
-export NCCL_SOCKET_IFNAME=enp25s0np0
-export NCCL_DEBUG=TRACE
+export SLURM_JOB_NAME=qwen3_235b_a22b_peft_8gpu_3node
+export NCCL_SOCKET_IFNAME=bond0
+# export NCCL_SOCKET_IFNAME=enp25s0np0
+export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=ALL
 export NVTE_FUSED_ATTN=0
 export NVTE_DEBUG=1
@@ -19,35 +20,21 @@ export NVTE_DEBUG_LEVEL=0
 export HYDRA_FULL_ERROR=1
 export VERL_LOGGING_LEVEL=DEBUG
 export VERL_SFT_LOGGING_LEVEL=DEBUG
-# Custom variables such as PATH / CUDA / NCCL
-export CUDA_DEVICE_MAX_CONNECTIONS=1
-
-# Cluster Network Setting
-export GPU_MAX_HW_QUEUES=2
-export TORCH_NCCL_HIGH_PRIORITY=1
-export NCCL_CHECKS_DISABLE=1
 export NCCL_IB_DISABLE=0
-export NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11
-export NCCL_IB_GID_INDEX=3
-export NCCL_CROSS_NIC=0
-export NCCL_PROTO=Simple
-export RCCL_MSCCL_ENABLE=0
-export TOKENIZERS_PARALLELISM=false
-
-# can make training faster, depends on your infrastructure
-export NCCL_IBEXT_DISABLE=1
-export NCCL_NVLS_ENABLE=1
-export UCX_NET_DEVICES=mlx5_0:1,mlx5_1:1,mlx5_2:1,mlx5_3:1,mlx5_4:1,mlx5_5:1,mlx5_6:1,mlx5_7:1,mlx5_8:1,mlx5_11:1
+export NCCL_IB_HCA=mlx5_0,mlx5_1,mlx5_2,mlx5_3,mlx5_4,mlx5_5,mlx5_6,mlx5_7,mlx5_8,mlx5_11,mlx5_bond_0
 
 conda activate $CONDA_PATH
 
 # distributed settings
+LOCAL_ADDR=osk-gpu68
+echo "LOCAL_ADDR=${LOCAL_ADDR}"
+NODE_RANK=2
+echo "Node rank: "$NODE_RANK
+
 MASTER_ADDR=osk-gpu66
 echo "MASTER_ADDR=${MASTER_ADDR}"
 MASTER_PORT=37171
 echo "MASTER_PORT=${MASTER_PORT}"
-NODE_RANK=2
-echo "Node rank: "$NODE_RANK
 NNODES=3
 echo "Node num: "$NNODES
 GPUS_PER_NODE=8
@@ -58,6 +45,7 @@ echo "Gpu num: "$GPUS_PER_NODE
 #export CUDA_VISIBLE_DEVICES=0
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export PYTHONUNBUFFERED=1
+export NCCL_DEBUG_FILE=train/logs/nccl-debug-${NODE_RANK}.log
 ulimit -v unlimited
 
 #YOU_TEAM_ENTITY_NAME を wandb の組織名に置き換えてください。
@@ -73,6 +61,7 @@ pid_nvsmi=$!
 
 torchrun --rdzv_backend c10d \
          --rdzv_endpoint ${MASTER_ADDR}:${MASTER_PORT} \
+         --local-addr ${LOCAL_ADDR} \
          --nnodes ${NNODES} --nproc_per_node ${GPUS_PER_NODE} \
          --node_rank ${NODE_RANK} \
          -m verl.trainer.fsdp_sft_trainer \
@@ -83,11 +72,11 @@ torchrun --rdzv_backend c10d \
          +data.response_dict_keys=['answer'] \
          data.train_batch_size=24 \
          data.micro_batch_size_per_gpu=1 \
-         model.partial_pretrain=deepseek-ai/DeepSeek-R1-0528 \
+         model.partial_pretrain=Qwen/Qwen3-235B-A22B \
          model.fsdp_config.model_dtype=bf16 \
          model.lora_rank=1 \
          model.lora_alpha=1 \
-         model.strategy=fsdp \
+         model.strategy=fsdp2 \
          data.max_length=1024 \
          use_remove_padding=True \
          ulysses_sequence_parallel_size=1 \
