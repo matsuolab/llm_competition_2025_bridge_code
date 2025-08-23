1255f5e7e0025b1b41b593ea2190060979db1efe
kumapo <kumapo@users.noreply.github.com>
2025-08-24 04:54:30 +0900

    need multinode_ppo

--- train/scripts/mutinode_ppo/ray_cluster.sh
+++ train/scripts/mutinode_ppo/ray_cluster.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=3
 #SBATCH --ntasks-per-node=2
 #SBATCH --gpus-per-node=8
-#SBATCH --cpus-per-task=64gt
+#SBATCH --cpus-per-task=64
 #SBATCH --time=6-00:00:00
 #SBATCH --mem=0
 #SBATCH --output=slurm-%j.out
